ec950b9634de491959d370cb1a95923c7bc64f75
Serpifon <104bsergio@gmail.com>
2016-05-18 19:40:43 +0200

    SE ACABA DE AÑADIR UN NUEVO BOTON

--- APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
+++ APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(361, 84);
+            this.button1.Location = new System.Drawing.Point(172, 84);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 102);
             this.button1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(77, 84);
+            this.button2.Location = new System.Drawing.Point(25, 84);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 194);
             this.button2.TabIndex = 1;
@@ -54,11 +55,21 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(327, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 205);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 501);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -71,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
